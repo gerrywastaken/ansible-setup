7e9735cc2ace798ad23b4f8fe2003892a97045c1
Gerard (Gerry) Caulfield <github.com@caulfield.me>
2012-12-01 01:31:12 +0800

    Original draft of setup.php

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
+#!/usr/bin/zsh
 # Install requirements
 sudo apt-get install openssh-server python-yaml python-paramiko python-jinja2
 
 #Install Ansible
 mkdir ~/gitRepos
-cd ~/gitRepos
-git clone git://github.com/ansible/ansible.git ~/ansible
-cd ./ansible
-source ./hacking/env-setup
-
+git clone git://github.com/ansible/ansible.git ~/gitRepos/ansible
+cd ~/gitRepos/ansible
+pwd
+echo `which source`
+source hacking/env-setup
 echo "127.0.0.1" > ~/ansible_hosts
 export ANSIBLE_HOSTS=~/ansible_hosts
 
 #Copy over ssh key to IP of the address you wish to configure
 ssh-copy-id 127.0.0.1
-
-
-
